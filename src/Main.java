6514869487ba052c70c4319fe9bdca0100685226
Smail <smail.jusic16@gmail.com>
2020-08-31 09:07:02 +0200

    Ready

---
+++ src/Main.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        int workTime;
+
+        System.out.println("Gib eine Zahl zwischen 0 & 24 ein");
+        workTime = sc.nextInt();
+
+        if(workTime == 12){
+            System.out.println("MITTAG :D");
+        }else if(workTime <= 16 && workTime >= 8){
+            System.out.println("Es ist während der Arbeitszeit");
+        }else{
+            System.out.println("Geh oabeitn");
+        }
+
+    }
+}
